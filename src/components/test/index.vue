351b1e567856275dbf828da0b2c5f7fc930fde63
Caleb <chenliangbo@yooleap.com>
2021-07-31 17:59:37 +0800

    feat: 新增练习题

---
+++ src/components/test/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3>题目描述：</h3>
+    <div>将arr数据</div>
+    <el-cascader :options="options" :props="props"></el-cascader>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [
+        { id: 1, name: "部门1", pid: 0 },
+        { id: 2, name: "部门2", pid: 1 },
+        { id: 3, name: "部门3", pid: 1 },
+        { id: 4, name: "部门4", pid: 3 },
+        { id: 5, name: "部门5", pid: 4 }
+      ],
+      props: {
+        value: "id",
+        label: "name"
+      },
+      options: []
+    };
+  },
+  mounted() {
+    this.changeDepartment();
+  },
+  methods: {
+    changeDepartment() {
+      while (this.arr.length > 1) {
+        let last = this.arr.pop();
+        for (let i = 0; i < this.arr.length; i++) {
+          if (last.pid === this.arr[i].id) {
+            if (this.arr[i].children) {
+              this.arr[i].children.push(last);
+            } else {
+              this.arr[i].children = [last];
+            }
+          }
+        }
+        this.options = this.arr;
+      }
+    }
+  }
+};
+</script>
